58f6d3e4dd9b4c01773f628f32268e808f4c321e
Bon Axl Feeser <bonaxl015@gmail.com>
2021-08-21 01:33:49 +0800

    Create Task model

---
+++ db/migrate/20210820172221_create_tasks.rb
@@ -0,0 +1,13 @@
+class CreateTasks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.text :description
+      t.integer :priority
+      t.datetime :deadline
+      t.boolean :completed
+
+      t.timestamps
+    end
+  end
+end
