bcf45e1afe9ae2e929d1538d1b22c4b852ff38f0
Bon Axl Feeser <bonaxl015@gmail.com>
2021-08-22 19:52:49 +0800

    Removed routes to show action

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'home/index'
   devise_for :users
-  resources :categories do
-    resources :tasks
+  resources :categories, except: %i[show] do
+    resources :tasks, except: %i[show]
   end
 end
